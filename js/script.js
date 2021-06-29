4c0d09e3fca034161144ab20e50f253bc7e3f5fd
Joseph Nyaga <joseph.kinyua@student.moringaschool.com>
2021-06-29 06:30:13 +0300

    This add a function that takes in the user input and perform arithmetic

--- js/script.js
+++ js/script.js
@@ -21,3 +21,36 @@ var pizzaPrices = [1100, 900, 750];
 var crustingCost = [100, 80, 55];
 var toppingCost = [80, 60, 40];
 var deliveryCost = [250, 0];
+$(document).ready(function() {
+    $("#myform").submit(function(event) {
+        event.preventDefault();
+        var pizzaType = $("#type").val();
+
+        var pizzaSize = parseInt($("#size").val());
+
+        var pizzaCrust = $("#crust").val();
+
+        var pizzaTopping = $("#topping").val();
+
+        var pizzaNumber = parseInt($("#num").val());
+
+        var pizzaDelivery = parseInt($("#mode").val());
+
+        var priceWithoutDelivery = pizzaPrices[pizzaSize] + toppingCost[pizzaTopping] + crustingCost[pizzaCrust];
+        var finalDelivery = deliveryCost[pizzaDelivery];
+
+        var myPizza = new pizzaDelicacy(pizzaType, pizzaSize, pizzaCrust, pizzaTopping);
+        var total2 = new Cost(finalDelivery, pizzaNumber, priceWithoutDelivery);
+        //  console.log(pizzaDelivery);
+        if (pizzaDelivery === 0) {
+            prompt("Enter the place where Your pizza will be delivered.");
+            alert("Thanks for placing an order which is " + myPizza.firstChoicePizza() + ". And total cost of your pizza is ksh " + total2.firstCost());
+
+        } else if (pizzaDelivery === 1) {
+
+            alert("Thanks for placing order which is " + myPizza.firstChoicePizza() + ". And total cost of your pizza is ksh " + total2.firstCost());
+
+        }
+
+    });
+});
